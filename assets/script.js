4019ee3a0563d09c81f1eadd3337b47569c9de9f
AileenAJJ <aileenjohnsonlsp@gmail.com>
2024-06-24 18:51:00 -0500

    changes to script file

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+// Interactivity with the header
+
+// Interactivity with the navbar
+
+// Interactity with the body
+
+// Interactivity with the footer
+
+
+
+// Code favorites feature
+
+// Create an array that the user can input values into
+
+// Save that array to localStorage
+
+// Have the user be able to retrieve that information from localStorage (Can the user retrieve it or it already just displayed?)
+
+
+
+// After the user searches for a holiday, next to the "go back" button they will see another button that says "save to favorites"
+
+// If they press this button the data will be saved to an array that will be saved to localStorage
+
+// This array will then be displayed in the "Favorites: " section
+
+// I'll need a for loop for the display code
