7d6e6fef3b0f4763f5a025316aeac451c51bd8ed
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-24 21:04:53 -0500

    Added pseudo code to weatherWidget.js

--- assets/weatherWidget.js
+++ assets/weatherWidget.js
@@ -3,4 +3,11 @@ const apiKey = '0c1d7915ad2662f0e450b432130b6989';
 const weatherQuery = `https://api.openweathermap.org/data/3.0/onecall?&appid=${apiKey}`;
 const geoQuery = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`;
 
-//
+// All of the following may be inside of a for loop in order to run the code three times on page load
+
+// Check if country is United States
+// If it is, run a query with state code to get lat and lon
+
+// Get lat and lon and pass it into the query with a placeholder date.
+
+// Generate one card per city and append it to an element on the page.
