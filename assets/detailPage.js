a49e178bb1b3c7f944347aaaa003dfff9ea7f299
AileenAJJ <aileenjohnsonlsp@gmail.com>
2024-06-24 21:17:50 -0500

    added heart button

--- assets/detailPage.js
+++ assets/detailPage.js
@@ -1,5 +1,10 @@
-/*Event listener for BackButton button*/
+// Event listener for BackButton button
 const element = document.getElementById("backBtn");
 element.addEventListener("click", function () {
   window.location.href = "landingPage.html";
 });
+
+// Makes the heart toggle yellow
+function toggleColor(button) {
+  button.classList.toggle('clicked');
+}
