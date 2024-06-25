a144c162e6ce7992896f2aafcda0ab2a1102566a
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-24 21:16:03 -0500

    Added placeholder data for weather query for testing

--- assets/weatherWidget.js
+++ assets/weatherWidget.js
@@ -1,9 +1,22 @@
 const apiKey = '0c1d7915ad2662f0e450b432130b6989';
 
 const weatherQuery = `https://api.openweathermap.org/data/3.0/onecall?&appid=${apiKey}`;
-const geoQuery = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`;
+// const geoQuery = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`;
 
+
+lat = 30;
+lon = -97
+
+date = '2025-12-25' //Placeholder until we can get the date from the holiday api
 // All of the following may be inside of a for loop in order to run the code three times on page load
+
+fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&date=${date}&appid=${apiKey}`)
+    .then(function(response) {
+        return response.json();
+    })
+        .then(function(data) {
+            console.log(data);
+        });
 
 // Check if country is United States
 // If it is, run a query with state code to get lat and lon
