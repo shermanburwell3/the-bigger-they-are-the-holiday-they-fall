39ccdb38d6d8842c60ca8a3c519cc576d19eb21d
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-25 19:34:37 -0500

    Added geo query api to weatherWidget.js

--- assets/weatherWidget.js
+++ assets/weatherWidget.js
@@ -1,21 +1,36 @@
 const apiKey = '0c1d7915ad2662f0e450b432130b6989';
+const countryObject = JSON.parse(localStorage.getItem('country'));
+const countryCode = countryObject.countryCode;
 
-const weatherQuery = `https://api.openweathermap.org/data/3.0/onecall?&appid=${apiKey}`;
+// const weatherQuery = `https://api.openweathermap.org/data/3.0/onecall/day_summary?&appid=${apiKey}`;
 // const geoQuery = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`;
 
 
 lat = 30;
 lon = -97
 
-date = '2025-12-25' //Placeholder until we can get the date from the holiday api
+date = '2025-12-10' //Placeholder until we can get the date from the holiday api
 // All of the following may be inside of a for loop in order to run the code three times on page load
+if (countryObject.countryCode == 'US') {
 
-fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&date=${date}&appid=${apiKey}`)
+    for (let i = 0; i < countryObject.cities.length; i++) {
+
+        let cityName = countryObject.cities[i];
+        let stateCode = countryObject.states[i];
+        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`)
+            .then(function (response) {
+                return response.json()
+            })
+    }
+}
+
+fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${date}&appid=${apiKey}`)
     .then(function(response) {
         return response.json();
     })
         .then(function(data) {
             console.log(data);
+            
         });
 
 // Check if country is United States
