72c3af5e969a32d5651c6da125591e30213b5c9c
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-25 20:36:20 -0500

    Weather widget appends elements to page

--- assets/weatherWidget.js
+++ assets/weatherWidget.js
@@ -55,7 +55,12 @@ function getWeather(index) {
 // Generate one card per city and append it to an element
 function createCards(weatherData, city){
 
+    // Get handle on the widget
+
+    const weatherDisplayEl = document.querySelector('#weatherForecastDisplay');
+
     // Create elements to add to widget
+
     const weatherCard = document.createElement('div');
     const cityEl = document.createElement('h3');
     const temperatureEl = document.createElement('p');
@@ -64,11 +69,22 @@ function createCards(weatherData, city){
     // Assign data into the elements
 
     cityEl.textContent = city;
-    temperatureEl.textContent = `High Temp: ${weatherData.temperature.max} F
-                                Low Temp: ${weatherData.temperature.min} F`
 
-    console.log(city);
-    console.log(weatherData);
+    temperatureEl.textContent =
+    `High Temp: ${Math.round(weatherData.temperature.max)} F
+    Low Temp: ${Math.round(weatherData.temperature.min)} F`
+
+
+    maxWindEl.textContent = `Max Wind Speed: ${Math.round(weatherData.wind.max.speed)} MPH`;
+
+    // Finally, append the cards to the widget
+
+    weatherCard.append(cityEl);
+    weatherCard.append(temperatureEl);
+    weatherCard.append(maxWindEl);
+
+    weatherDisplayEl.append(weatherCard);
+   
 
 
 }
