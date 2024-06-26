cd3a513bd48f37b528b63ae7900a8319a221605f
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-25 19:46:29 -0500

    lat and lon feed into new getWeather function and returns the weather for each area on the specified date

--- assets/weatherWidget.js
+++ assets/weatherWidget.js
@@ -1,13 +1,11 @@
 const apiKey = '0c1d7915ad2662f0e450b432130b6989';
 const countryObject = JSON.parse(localStorage.getItem('country'));
-const countryCode = countryObject.countryCode;
 
 // const weatherQuery = `https://api.openweathermap.org/data/3.0/onecall/day_summary?&appid=${apiKey}`;
 // const geoQuery = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`;
 
-
-lat = 30;
-lon = -97
+const lat = [];
+const lon = [];
 
 date = '2025-12-10' //Placeholder until we can get the date from the holiday api
 // All of the following may be inside of a for loop in order to run the code three times on page load
@@ -17,21 +15,35 @@ if (countryObject.countryCode == 'US') {
 
         let cityName = countryObject.cities[i];
         let stateCode = countryObject.states[i];
+        const countryCode = countryObject.countryCode;
+
         fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`)
             .then(function (response) {
                 return response.json()
             })
+                .then(function (data) {
+                    // Assign global lat and lon the first lat and lon result of each search
+                    lat.push(data[0].lat);
+                    lon.push(data[0].lon);
+                    console.log(data);
+                    console.log(lat);
+                    console.log(lon);
+                    getWeather(i);
+                })
     }
 }
 
-fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${date}&appid=${apiKey}`)
-    .then(function(response) {
-        return response.json();
-    })
-        .then(function(data) {
-            console.log(data);
-            
-        });
+
+function getWeather(index) {
+    fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat[index]}&lon=${lon[index]}&date=${date}&appid=${apiKey}`)
+        .then(function(response) {
+            return response.json();
+        })
+            .then(function(data) {
+                console.log(data);
+                
+            });
+}
 
 // Check if country is United States
 // If it is, run a query with state code to get lat and lon
