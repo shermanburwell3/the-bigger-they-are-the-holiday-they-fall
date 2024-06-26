f9c5f74d88cd5ac7e7c45d494a73b91488ef2269
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-25 20:20:49 -0500

    Finished getWeather function and created createCards function

--- assets/weatherWidget.js
+++ assets/weatherWidget.js
@@ -4,12 +4,18 @@ const countryObject = JSON.parse(localStorage.getItem('country'));
 // const weatherQuery = `https://api.openweathermap.org/data/3.0/onecall/day_summary?&appid=${apiKey}`;
 // const geoQuery = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`;
 
+// Declare arrays for lat and lon glabally
 const lat = [];
 const lon = [];
 
+
 date = '2025-12-10' //Placeholder until we can get the date from the holiday api
 // All of the following may be inside of a for loop in order to run the code three times on page load
+
+// Check if country is United States
 if (countryObject.countryCode == 'US') {
+
+    // If it is, run a query with state code to get lat and lon
 
     for (let i = 0; i < countryObject.cities.length; i++) {
 
@@ -25,29 +31,51 @@ if (countryObject.countryCode == 'US') {
                     // Assign global lat and lon the first lat and lon result of each search
                     lat.push(data[0].lat);
                     lon.push(data[0].lon);
-                    console.log(data);
-                    console.log(lat);
-                    console.log(lon);
+                    // Pass our current index into getWeather function to use on the lat and lon arrays later
                     getWeather(i);
                 })
     }
 }
 
-
+// Get weather data using lat and lon
 function getWeather(index) {
-    fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat[index]}&lon=${lon[index]}&date=${date}&appid=${apiKey}`)
+    
+    fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat[index]}&lon=${lon[index]}&date=${date}&units=imperial&appid=${apiKey}`)
         .then(function(response) {
             return response.json();
         })
             .then(function(data) {
                 console.log(data);
+
+                createCards(data, countryObject.cities[index]);
                 
             });
 }
 
-// Check if country is United States
-// If it is, run a query with state code to get lat and lon
+// Generate one card per city and append it to an element
+function createCards(weatherData, city){
 
-// Get lat and lon and pass it into the query with a placeholder date.
+    // Create elements to add to widget
+    const weatherCard = document.createElement('div');
+    const cityEl = document.createElement('h3');
+    const temperatureEl = document.createElement('p');
+    const maxWindEl = document.createElement('p');
 
-// Generate one card per city and append it to an element on the page.
+    // Assign data into the elements
+
+    cityEl.textContent = city;
+    temperatureEl.textContent = `High Temp: ${weatherData.temperature.max} F
+                                Low Temp: ${weatherData.temperature.min} F`
+
+    console.log(city);
+    console.log(weatherData);
+
+
+}
+
+
+
+
+
+
+
